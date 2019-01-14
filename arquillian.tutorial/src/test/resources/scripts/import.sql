d917ee858b825a16a30574ea0331ff0b3b4489b4
Lavinia <masini.lavinia@gmail.com>
2019-01-14 13:17:36 +0100

    Added files to be used with Arquillian IT

---
+++ arquillian.tutorial/src/test/resources/scripts/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO products(id, category, description, name, price) VALUES (1, 'category1', 'description1', 'name1', 1.00);
+INSERT INTO products(id, category, description, name, price) VALUES (2, 'category2', 'description2', 'name2', 2.00);
+INSERT INTO users(id, address, email, firstname, lastname, password, username) VALUES (1, 'address', 'email@email.com', 'firstname', 'lastname', 'password', 'username');
+
