7b206c68c4aa86968f482b3f72d9ba977d5d644e
Lavinia <masini.lavinia@gmail.com>
2019-01-20 14:12:24 +0100

    Updated script

--- scripts/script.sh
+++ scripts/script.sh
@@ -10,12 +10,9 @@ download_jboss () {
 }
 
 if [ ! -d "$JBOSS_HOME" ]; then
-    echo "JBoss dir doesn't exist, downloading wildfly! ( $JBOSS_HOME )"
     download_jboss
 else
-    echo "JBoss dir exists ( $JBOSS_HOME )"
     if [ `ls "$JBOSS_HOME" | wc -l` -eq 0 ]; then
-        echo "... but is empty, downloading wildfly!"
         download_jboss
     fi
 fi
