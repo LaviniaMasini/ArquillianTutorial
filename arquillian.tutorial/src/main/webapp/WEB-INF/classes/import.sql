b07b1a44fe95ff5ba72b01475b2fb9a2b8935c49
Lavinia <masini.lavinia@gmail.com>
2019-01-21 10:04:15 +0100

    Added import.sql script

---
+++ arquillian.tutorial/src/main/webapp/WEB-INF/classes/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO products(id, category, description, name, price) VALUES (1, 'category1', 'description1', 'name1', 1.00);
+INSERT INTO products(id, category, description, name, price) VALUES (2, 'category2', 'description2', 'name2', 2.00);
+
