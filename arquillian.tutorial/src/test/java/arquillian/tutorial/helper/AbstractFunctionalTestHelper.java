4df2a6d0bd03a72fd219b1e18fce81ddb599cbc8
Lavinia <masini.lavinia@gmail.com>
2019-01-22 19:00:47 +0100

    Minor Changes

--- arquillian.tutorial/src/test/java/arquillian/tutorial/helper/AbstractFunctionalTestHelper.java
+++ arquillian.tutorial/src/test/java/arquillian/tutorial/helper/AbstractFunctionalTestHelper.java
@@ -17,7 +17,7 @@ public class AbstractFunctionalTestHelper {
 	protected WebDriver browser;
 	@ArquillianResource
 	protected URL url;
-	
+
 	protected static final String WEBAPP_SRC = "src/main/webapp";
 
 	public AbstractFunctionalTestHelper() {
@@ -27,7 +27,7 @@ public class AbstractFunctionalTestHelper {
 	protected void buildURL(String path) {
 		browser.get(url.toExternalForm().replaceAll(path, "").concat("arquillian.tutorial/"));
 	}
-	
+
 	protected void assertEmptyFieldMessage(String field, String attribute, String msg) {
 		Actions action = new Actions(browser);
 		WebElement webElement = browser.findElement(By.id(field));
@@ -35,11 +35,11 @@ public class AbstractFunctionalTestHelper {
 		String message = webElement.getAttribute(attribute);
 		assertEquals(msg, message);
 	}
-	
+
 	protected void assertUserSummaryPage(String username) {
 		assertEquals("User Home", browser.getTitle());
-		assertEquals("Welcome "+username, browser.findElement(By.id("welcomeTitle")).getText());
-		assertEquals("Welcome "+username, browser.findElement(By.id("welcomeBtn")).getText());
+		assertEquals("Welcome " + username, browser.findElement(By.id("welcomeTitle")).getText());
+		assertEquals("Welcome " + username, browser.findElement(By.id("welcomeBtn")).getText());
 		browser.findElement(By.id("welcomeBtn")).click();
 		assertEquals("User Info", browser.findElement(By.id("userInfo")).getText());
 		assertEquals("Logout", browser.findElement(By.id("logout")).getText());
